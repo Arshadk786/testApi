22cbba8d1cd6f08bce9eef30b16673a3cdd08b4c
Arshad Khan <arshadkhan7867865422@gmail.com>
2023-09-08 00:53:10 +0530

    test api

---
+++ testapi.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Text(BaseModel):
+    text : str
+
+@app.get("/")
+async def index():
+    return {"message": "Welcome to the API"}
+
+@app.post("/test")
+async def predict(request : Text):
+    return request
